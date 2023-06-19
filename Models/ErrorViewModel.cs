c19900a2f7c9265c138244b267fe5bed28e410f7
vcorp <vcorp@VCORP-PC>
2023-06-20 01:46:30 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ITSAIntermediate_VelaphiMhlanga.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
