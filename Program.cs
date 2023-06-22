9915975bb21c46a3791b6ec3b979383b80d674a5
velly petrus <velly.vcorporation@gmail.com>
2023-06-22 20:09:53 +0200

    performed migration of Identity Tables creation as IdentityTables

--- Program.cs
+++ Program.cs
@@ -30,6 +30,8 @@ app.UseAuthentication(); ;
 
 app.UseAuthorization();
 
+app.MapRazorPages();
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
